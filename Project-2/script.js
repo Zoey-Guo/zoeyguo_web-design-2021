a5b51841590a06dd71ab11cb699d37af9f9a90ac
Zoey-Guo <61063207+Zoey-Guo@users.noreply.github.com>
2021-02-08 11:54:07 -0500

    Final

--- Project-2/script.js
+++ Project-2/script.js
@@ -1,8 +1,13 @@
 $("document").ready(function() {
    $(".item").mouseenter(function() {
      $(this).css("background-color","blue");
+     $(this).css("color","yellow");
+  
    });
    $(".item").mouseleave(function() {
      $(this).css("background-color","black");
+     $(this).css("color","white");
    });
+
+
   });
